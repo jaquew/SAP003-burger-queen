741b9eeda5a80dcd4691d773873563554c1f4c73
jaquew <eng.jaquesilva@gmail.com>
2019-12-17 19:45:46 -0300

    menu comonent added

---
+++ src/components/Breakfast/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Button from '../Button'
+import { StyleSheet, css } from 'aphrodite';
+
+const styles =  StyleSheet.create( {
+  btnlayout: {
+    display:"flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexWrap: "wrap",
+    minHeight: "70px"
+  },
+  menubtn: {
+    backgroundColor: "#fff",
+    fontFamily: "Arial",
+    width: "60%",
+    height: "50px",
+    border: "1px solid #c44",
+    borderRadius: "15px",
+    ':active': {
+      backgroundColor: "#25B6D2",
+      color: "#fff"
+    },
+  },
+  btnlabel: {
+    maxWidth: "35%",
+    textAlign: "right"
+  },
+})
+const Breakfast = ({breakfast, addOrder}) => {
+
+  return(
+    <section>
+      <h3>Café da manhã</h3>
+      {breakfast.map((item)=> (
+        <div className={css(styles.btnlayout)}>
+          <Button className={css(styles.menubtn)} title={item.name} id={item.id} handleclick={() => addOrder(item)}/>
+          <label className={css(styles.btnlabel)} htmlFor={item.id}>{'R$ ' + item.price +',00'}</label>
+        </div>
+      ))}
+    </section>
+  )
+}
+
+export default Breakfast
